558201a3bf32eb89eb1b581456736ea2c81102c8
Jacob Sanders <jacosand@gmail.com>
2026-05-19 20:42:35 -0400

    Clean up bpe training

--- tokenizer.py
+++ tokenizer.py
@@ -1,60 +1,67 @@
+import os
+
 input_string = "low low low low low lower lower widest widest widest newest newest newest newest newest newest"
 
 
-def find_merge_pair(freq_table):
-    pair_counts = {}
-    for k, v in freq_table.items():
-        for pair in zip(k, k[1:]):
-            pair_counts[pair] = pair_counts.get(pair, 0) + v
+def find_merge_pair(pretoken_freq):
+    pair_freq = {}
+    for pretoken, freq in pretoken_freq.items():
+        for pair in zip(pretoken, pretoken[1:]):
+            pair_freq[pair] = pair_freq.get(pair, 0) + freq
 
-    return max(pair_counts, key = lambda pair: (pair_counts[pair], pair))
+    return max(pair_freq, key = lambda pair: (pair_freq[pair], pair))
 
-def merge(freq_table, pair, idx):
-    new_freq_table = {}
 
-    for k, v in freq_table.items():
+def merge(pretoken_freq, pair, idx):
+    merged_pretoken_freq = {}
+
+    for pretoken, freq in pretoken_freq.items():
         i = 0
-        new_k = k
-        while i < len(k) - 1:
-            if (k[i], k[i+1]) == pair:
-                new_k = (*new_k[:i], idx, *new_k[i+2:])
+        new_pretoken = pretoken
+        while i < len(pretoken) - 1:
+            if (pretoken[i], pretoken[i+1]) == pair:
+                new_pretoken = (*new_pretoken[:i], idx, *new_pretoken[i+2:])
                 i += 2
             else:
                 i += 1
-        new_freq_table[new_k] = freq_table[k]
+        merged_pretoken_freq[new_pretoken] = pretoken_freq[pretoken]
 
-    return new_freq_table
+    return merged_pretoken_freq
+
 
 def train_bpe(
-    input_path: str,
+    input_path: str | os.PathLike,
     vocab_size: int,
-    special_tokens: list[str]
-    ) -> (dict[int, bytes], list[tuple[bytes, bytes]]):
+    special_tokens: list[str],
+    ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
 
-    pre_tokens = input_path.split()
+    n_special_tokens = len(special_tokens)
+    n_single_bytes = 256
 
     vocab = {}
-    for i in range(256):
-        vocab[i] = chr(i).encode('utf-8')
-
-    freq_table = {}
-    for pt in pre_tokens:
-        pt_bytes = tuple(pt.encode('utf-8'))
-        freq_table[pt_bytes] = freq_table.get(pt_bytes, 0) + 1
-    
-    merge_pair = find_merge_pair(freq_table)
-
-    for i in range(256, vocab_size):
-        merge_pair = find_merge_pair(freq_table)
-        freq_table = merge(freq_table, merge_pair, i)
-
-    #vocab[i] = b"".join(chr(c).encode('utf-8') for c in merge_pair)
-    #print(vocab)
-
     merges = []
+    for i in range(n_special_tokens):
+        vocab[i] = special_tokens[i].encode('utf-8')
+    for i in range(n_single_bytes):
+        vocab[n_special_tokens + i] = chr(i).encode('utf-8')
 
+    pretokens = input_path.split()
+    pretoken_freq = {}
+    for pretoken in pretokens:
+        pretoken_tuple = tuple(pretoken.encode('utf-8'))
+        pretoken_freq[pretoken_tuple] = pretoken_freq.get(pretoken_tuple, 0) + 1
+    
+    for i in range(n_special_tokens + n_single_bytes, vocab_size):
+        int1, int2 = find_merge_pair(pretoken_freq)
+        byte1 = vocab[int1 + n_special_tokens]
+        byte2 = vocab[int2 + n_special_tokens]
+        merges.append((byte1, byte2))
+        pretoken_freq = merge(pretoken_freq, (int1, int2), i)
+        vocab[i + n_special_tokens] = byte1 + byte2
 
     return vocab, merges
 
 if __name__ == "__main__":
-    train_bpe(input_string, 256+6, [])
+    vocab, merges = train_bpe(input_string, 263, ["<|endoftext|>"])
+    print(vocab)
+    print(merges)
