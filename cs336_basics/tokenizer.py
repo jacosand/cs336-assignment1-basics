c87372b440a39a4145b4bf282be80881d0f878b8
Jacob Sanders <jacosand@gmail.com>
2026-05-31 10:37:56 -0400

    First version of encode and decode

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,27 +1,97 @@
 from typing import Iterable, Iterator
+import pickle
+import regex as re
+
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
 
 class Tokenizer:
+
     def __init__(
         self,
         vocab: dict[int, bytes],
         merges: list[tuple[bytes, bytes]],
         special_tokens: list[str] | None = None
     ):
-        pass
+        self.vocab = vocab
+        self.merges = merges
+        self.special_tokens = special_tokens
 
+        self.token_ids = {token_bytes: token_id for token_id, token_bytes in self.vocab.items()}
+
+
+    @classmethod
     def from_files(
         cls,
         vocab_filepath: str,
         merges_filepath: str,
         special_tokens: list[str] | None = None
     ):
-        pass
+        with open(vocab_filepath, "rb") as f:
+            vocab = pickle.load(f)
+        with open(merges_filepath, "rb") as f:
+            merges = pickle.load(f)
+        return cls(vocab, merges, special_tokens)
+
 
     def encode(self, text: str) -> list[int]:
-        pass
+        
+        tokens = []
+
+        if self.special_tokens:
+            split_pattern = "|".join(re.escape(special_token) for special_token in self.special_tokens)
+            docs = re.split(f"({split_pattern})", text)
+        else:
+            docs = [text]
+
+        for doc in docs:
+            if self.special_tokens and doc in self.special_tokens:
+                tokens.append(self.token_ids[doc.encode("utf-8")])
+            else:
+                for pretoken in re.finditer(PAT, doc):
+                    pretoken_bytes = [bytes([b]) for b in pretoken.group().encode("utf-8")]
+                    new_pretoken_bytes = []
+
+                    for merge in self.merges:
+                        pretoken_index = 0
+                        while pretoken_index <= len(pretoken_bytes) - 1:
+                            if pretoken_index < len(pretoken_bytes) - 1 and tuple(pretoken_bytes[pretoken_index:pretoken_index+2]) == merge:
+                                new_pretoken_bytes.append(pretoken_bytes[pretoken_index] + pretoken_bytes[pretoken_index+1])
+                                pretoken_index += 2
+                            else:
+                                new_pretoken_bytes.append(pretoken_bytes[pretoken_index])
+                                pretoken_index += 1
+                        pretoken_bytes = new_pretoken_bytes
+                        new_pretoken_bytes = []
+            
+                    tokens += [self.token_ids[b] for b in pretoken_bytes]
+
+        return tokens
+
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
         pass
 
+
     def decode(self, ids: list[int]) -> str:
-        pass
+        bytes_string = b""
+        for i in ids:
+            bytes_string += self.vocab[i]
+        
+        return bytes_string.decode("utf-8", errors="replace")
+
+
+def main():
+    tokenizer = Tokenizer.from_files(
+        vocab_filepath = "artifacts/tokenizer-tinystories-10000-vocab.pkl",
+        merges_filepath = "artifacts/tokenizer-tinystories-10000-merges.pkl",
+        special_tokens = None,
+    )
+    
+    encoding = tokenizer.encode("hello")
+    print(encoding)
+    decoding = tokenizer.decode(encoding)
+    print(decoding)
+
+if __name__ == "__main__":
+    main()
