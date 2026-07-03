4f30270f657e00ffd6e1c57c3d5be8c45eadefda
Jacob Sanders <jacosand@gmail.com>
2026-07-03 09:45:04 -0400

    Implement Embedding

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -15,6 +15,9 @@ class Linear(nn.Module):
     ):
         super().__init__()
         
+        self.in_features = in_features
+        self.out_features = out_features
+
         sigma = (2 / (in_features + out_features)) ** 0.5
 
         self.weight = nn.Parameter(torch.empty(out_features, in_features, dtype=dtype, device=device))
@@ -26,3 +29,28 @@ class Linear(nn.Module):
         x: Float[Tensor, "... in_features"],
     ) -> Float[Tensor, "... out_features"]:
         return einsum(x, self.weight, '... in_features, out_features in_features -> ... out_features')
+
+
+class Embedding(nn.Module):
+
+    def __init__(
+        self,
+        num_embeddings: int,
+        embedding_dim: int,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ):
+        super().__init__()
+
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+
+        self.weight = nn.Parameter(torch.empty(num_embeddings, embedding_dim, dtype=dtype, device=device))
+
+        nn.init.trunc_normal_(self.weight, mean=0.0, std=1, a=-3, b=3)
+    
+    def forward(
+        self,
+        token_ids: Int[Tensor, "..."],
+    ) -> Float[Tensor, "... embedding_dim"]:
+        return self.weight[token_ids]
