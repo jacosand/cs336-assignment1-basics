deee160351689642d4a5f3d7ce6876016aee1502
Jacob Sanders <jacosand@gmail.com>
2026-07-07 08:45:04 -0400

    Add cross entropy function

--- cs336_basics/nn_utils.py
+++ cs336_basics/nn_utils.py
@@ -1,5 +1,5 @@
 import torch
-from jaxtyping import Float
+from jaxtyping import Float, Int
 from torch import Tensor
 
 
@@ -9,3 +9,16 @@ def softmax(x: Float[Tensor, '...'], dim: int) -> Float[Tensor, '...']:
     y = torch.exp(y)
 
     return y / y.sum(dim=dim, keepdim=True)
+
+
+def cross_entropy(
+    logits: Float[Tensor, "... vocab_size"],
+    targets: Int[Tensor, "..."],
+    ) -> float:
+
+    shifted_logits = logits - logits.max(dim=-1, keepdim=True).values
+    target_logits = torch.gather(
+        shifted_logits, dim=-1, index=targets.unsqueeze(-1),
+    ).squeeze(-1)
+
+    return torch.mean(torch.logsumexp(shifted_logits, dim=-1) - target_logits)
