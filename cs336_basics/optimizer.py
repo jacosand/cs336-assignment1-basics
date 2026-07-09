ec9aec38a383b55f4aa1428ffb2b8f12fec867c7
Jacob Sanders <jacosand@gmail.com>
2026-07-09 17:52:12 -0400

    Add learning rate schedule

--- cs336_basics/optimizer.py
+++ cs336_basics/optimizer.py
@@ -89,3 +89,20 @@ class AdamW(torch.optim.Optimizer):
                 state["v"] = v
 
         return loss
+
+
+def get_lr_cosine_schedule(
+    it: int,
+    max_learning_rate: float,
+    min_learning_rate: float,
+    warmup_iters: int,
+    cosine_cycle_iters: int,
+) -> float:
+    
+    if it < warmup_iters:
+        return it * max_learning_rate / warmup_iters
+    elif it < cosine_cycle_iters:
+        cosine_frac = (it - warmup_iters) / (cosine_cycle_iters - warmup_iters)
+        return min_learning_rate + 0.5 * (max_learning_rate-min_learning_rate) * (1 + math.cos(cosine_frac * math.pi))
+    else:
+        return min_learning_rate
